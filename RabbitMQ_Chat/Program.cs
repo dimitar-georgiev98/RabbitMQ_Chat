f35485baae7b38bfef66b8bf54b4e142586403f1
dimitar_georgiev <dimitar.georgiev99@abv.bg>
2023-01-23 16:23:46 +0200

    code clean up

--- RabbitMQ_Chat/Program.cs
+++ RabbitMQ_Chat/Program.cs
@@ -37,68 +37,10 @@ namespace RabbitMQ_Chat
                         var body = Encoding.UTF8.GetBytes(message);
                         channel.BasicPublish("direct", "msg", basicProperties, body);
                     }
-
                 };
                 channel.BasicConsume("msgq", false, basicConsumer);
                 Console.ReadLine();
             }
-
-            //RunQueue();
-        }
-
-        private static void RunQueue()
-        {
-            ConnectionFactory connectionFactory = new ConnectionFactory();
-
-            connectionFactory.Port = 5672;
-            connectionFactory.HostName = "localhost";
-            connectionFactory.UserName = "account";
-            connectionFactory.Password = "accountpass";
-            connectionFactory.VirtualHost = "test";
-
-            IConnection connection = connectionFactory.CreateConnection();
-            IModel channel = connection.CreateModel();
-
-            channel.QueueDeclare("queues.msg", true, false, false, null);
-            SendMessage(channel);
-
-            channel.Close();
-            connection.Close();
-        }
-
-        private static void SendMessage(IModel channel)
-        {
-            string responseQueue = channel.QueueDeclare().QueueName;
-
-            string correlationId = Guid.NewGuid().ToString();
-            string consumerResponse = null;
-
-            IBasicProperties basicProperties = channel.CreateBasicProperties();
-            basicProperties.ReplyTo = responseQueue;
-            basicProperties.CorrelationId = correlationId;
-            Console.Write("Enter your message: ");
-            string message = Console.ReadLine();
-            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
-            channel.BasicPublish("", "queues.msg", basicProperties, messageBytes);
-
-            EventingBasicConsumer eventingBasicConsumer = new EventingBasicConsumer(channel);
-            eventingBasicConsumer.Received += (sender, basicDeliveryEventArgs) =>
-            {
-                IBasicProperties props = basicDeliveryEventArgs.BasicProperties;
-                if (props != null && props.CorrelationId == correlationId)
-                {
-                    var body = basicDeliveryEventArgs.Body.ToArray();
-                    string response = Encoding.UTF8.GetString(body);
-                    consumerResponse = response;
-                }
-                channel.BasicAck(basicDeliveryEventArgs.DeliveryTag, false);
-                Console.WriteLine("Response: {0}", consumerResponse);
-                Console.Write("Enter your message: ");
-                message = Console.ReadLine();
-                messageBytes = Encoding.UTF8.GetBytes(message);
-                channel.BasicPublish("", "queues.msg", basicProperties, messageBytes);
-            };
-            channel.BasicConsume(responseQueue, false, eventingBasicConsumer);
         }
     }
 }
--- RabbitMQ_Chat_Consumer/Program.cs
+++ RabbitMQ_Chat_Consumer/Program.cs
@@ -16,47 +16,17 @@ namespace RabbitMQ_Chat_Consumer
                 Console.WriteLine("No messages yet!");
 
                 var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (sender, BasicDeliverEventArgs) =>
+                consumer.Received += (sender, BasicDeliveryEventArgs) =>
                 {
-                    var body = BasicDeliverEventArgs.Body.ToArray();
+                    var body = BasicDeliveryEventArgs.Body.ToArray();
                     var message = Encoding.UTF8.GetString(body);
-                    channel.BasicAck(BasicDeliverEventArgs.DeliveryTag, false);
-                    var routingKey = BasicDeliverEventArgs.RoutingKey;
+                    channel.BasicAck(BasicDeliveryEventArgs.DeliveryTag, false);
+                    var routingKey = BasicDeliveryEventArgs.RoutingKey;
                     Console.WriteLine("Message received: {0}", message);
-                    Console.Write("Enter your reply: ");
                 };
                 channel.BasicConsume("msgq", false, consumer);
                 Console.ReadLine();
             }
-
-            /*
-            ConnectionFactory connectionFactory = new ConnectionFactory();
-
-            connectionFactory.Port = 5672;
-            connectionFactory.HostName = "localhost";
-            connectionFactory.UserName = "account";
-            connectionFactory.Password = "accountpass";
-            connectionFactory.VirtualHost = "test";
-
-            IConnection connection = connectionFactory.CreateConnection();
-            IModel channel = connection.CreateModel();
-            channel.BasicQos(0, 1, false);
-
-            EventingBasicConsumer eventingBasicConsumer = new EventingBasicConsumer(channel);
-            eventingBasicConsumer.Received += (sender, basicDeliveryEventArgs) =>
-            {
-                var body = basicDeliveryEventArgs.Body.ToArray();
-                string message = Encoding.UTF8.GetString(body);
-                channel.BasicAck(basicDeliveryEventArgs.DeliveryTag, false);
-                Console.Write("Message: {0} {1}", message, "Enter your message: ");
-                string response = Console.ReadLine();
-                IBasicProperties basicProperties = channel.CreateBasicProperties();
-                basicProperties.CorrelationId = basicDeliveryEventArgs.BasicProperties.CorrelationId;
-                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-                channel.BasicPublish("", basicDeliveryEventArgs.BasicProperties.ReplyTo, basicProperties, responseBytes);
-            };
-            channel.BasicConsume("queues.msg", false, eventingBasicConsumer);
-            */
         }
     }
 }
